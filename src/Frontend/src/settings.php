697ef934db1fcb762f3f849699587e04a052ef4e
Austin Selick-Bottos <segfault24@users.noreply.github.com>
2017-12-26 01:04:23 -0500

    started adding insurance endpoints; some php formatting

--- src/Frontend/src/settings.php
+++ src/Frontend/src/settings.php
@@ -3,17 +3,17 @@ return [
     'settings' => [
         'displayErrorDetails' => false, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-
+                                           
         // Renderer settings
         'renderer' => [
-            'template_path' => __DIR__ . '/../templates/',
+            'template_path' => __DIR__ . '/../templates/'
         ],
-
+        
         // Monolog settings
         'logger' => [
             'name' => 'slim-app',
             'path' => __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
-    ],
+            'level' => \Monolog\Logger::DEBUG
+        ]
+    ]
 ];
